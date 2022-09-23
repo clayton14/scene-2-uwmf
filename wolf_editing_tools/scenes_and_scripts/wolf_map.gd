881fb2968337a64667c395321c142477c54629a3
Jason Yundt <jason@jasonyundt.email>
2022-09-23 17:51:31 -0400

    Create WolfMap class

---
+++ wolf_editing_tools/scenes_and_scripts/wolf_map.gd
@@ -0,0 +1,6 @@
+extends Object
+
+# This will be the name of the header lump [1] when the map is exported.
+#
+# [1]: <https://github.com/rheit/zdoom/blob/d44976175256f3db8ec61cca40f1267cca68967d/specs/udmf.txt#L161>
+export var internal_name := "MAP01"
