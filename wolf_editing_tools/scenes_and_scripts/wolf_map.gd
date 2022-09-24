9ac48eb38304899cee2d7ea1de5fa782c05fc4e0
Jason Yundt <jason@jasonyundt.email>
2022-09-23 21:15:50 -0400

    Start actually using internal_name

--- wolf_editing_tools/scenes_and_scripts/wolf_map.gd
+++ wolf_editing_tools/scenes_and_scripts/wolf_map.gd
@@ -2,7 +2,8 @@ extends Node
 
 const Wad := preload("res://wolf_editing_tools/scenes_and_scripts/wad.gd")
 
-# This will be the name of the header lump [1] when the map is exported.
+# This will be the name of the header lump [1] when the map is exported. It also gets used as the
+# basename of the WAD file.
 #
 # [1]: <https://github.com/rheit/zdoom/blob/d44976175256f3db8ec61cca40f1267cca68967d/specs/udmf.txt#L161>
 export var internal_name := "MAP01"
@@ -10,10 +11,6 @@ export var internal_name := "MAP01"
 
 # For the moment, I’m going to make _ready() export the map.
 func _ready() -> void:
-	var name : = "01234567"
-	var contents := PoolByteArray()
-	
-	var lump := Wad.Lump.new(name, contents)
 	var wad := Wad.new()
-	assert(wad.append_lump(lump), "Appending lump failed.")
-	assert(wad.save("user://TEST.WAD") == OK, "Saving WAD failed.")
+	assert(wad.append_lump(Wad.Lump.new(internal_name, PoolByteArray())), "Appending lump failed.")
+	assert(wad.save("user://%s.WAD" % [internal_name]) == OK, "Saving WAD failed.")
