308b075b3f83f21b89af435995cabe960932240d
Jason Yundt <jason@jasonyundt.email>
2022-09-27 18:39:11 -0400

    Create Wall scene
    Later, I’ll create a Tile scene that will consist of 4 walls. Levels
    will be constructed using Tiles.

---
+++ project/wolf_editing_tools/scenes_and_scripts/map/wall.gd
@@ -0,0 +1,17 @@
+extends MeshInstance
+
+
+const DEFAULT_TEXTURE_PATH := "res://wolf_editing_tools/generated/missing_texture.tex"
+export var texture : Texture = preload(DEFAULT_TEXTURE_PATH) setget set_texture
+
+
+func set_texture(new_texture : Texture) -> void:
+	texture = new_texture
+	var material := SpatialMaterial.new()
+	material.flags_unshaded = true
+	material.albedo_texture = new_texture
+	set_surface_material(0, material)
+
+
+func _ready() -> void:
+	set_texture(texture)
