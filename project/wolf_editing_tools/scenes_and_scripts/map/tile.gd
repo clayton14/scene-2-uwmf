dad5f4bab2da589ba710169347c579c532d85bc7
Jason Yundt <jason@jasonyundt.email>
2022-09-28 19:51:49 -0400

    Refactor Tile to only use one mesh
    Each item in a MeshLibrary can consist of only one mesh. This change
    will hopefully allow us to create a MeshLibrary that contains Tiles.

--- project/wolf_editing_tools/scenes_and_scripts/map/tile.gd
+++ project/wolf_editing_tools/scenes_and_scripts/map/tile.gd
@@ -1,35 +1,88 @@
 tool
-extends Spatial
+extends MeshInstance
 
 
-const Wall := preload("res://wolf_editing_tools/scenes_and_scripts/map/wall.gd")
+const VSwap := preload("res://wolf_editing_tools/scenes_and_scripts/file_formats/v_swap.gd")
+const MISSING_TEXTURE_PATH := "res://wolf_editing_tools/generated/art/missing_texture.tex"
+const IMAGE_FORMAT := Image.FORMAT_RGB8
 
-export var texture_east : Texture = Wall.DEFAULT_TEXTURE setget set_texture_east, get_texture_east
-export var texture_north : Texture = Wall.DEFAULT_TEXTURE setget set_texture_north, get_texture_north
-export var texture_south : Texture = Wall.DEFAULT_TEXTURE setget set_texture_south, get_texture_south
-export var texture_west : Texture = Wall.DEFAULT_TEXTURE setget set_texture_west, get_texture_west
+export(String, FILE, "*.tex") var texture_east_path : String = MISSING_TEXTURE_PATH setget set_texture_east_path
+export(String, FILE, "*.tex") var texture_north_path : String = MISSING_TEXTURE_PATH setget set_texture_north_path
+export(String, FILE, "*.tex") var texture_south_path : String = MISSING_TEXTURE_PATH setget set_texture_south_path
+export(String, FILE, "*.tex") var texture_west_path : String = MISSING_TEXTURE_PATH setget set_texture_west_path
 
 
-func set_texture_east(new_texture_east : Texture) -> void:
-	$East.texture = new_texture_east
+func effective_automap_texture_path() -> String:
+	return texture_east_path
 
-func get_texture_east() -> Texture:
-	return $East.texture
+func _update_material() -> void:
+	var albedo_image := Image.new()
+	# TODO: What are mipmaps? Should use_mipmaps be true?
+	albedo_image.create(VSwap.WALL_LENGTH * 3, VSwap.WALL_LENGTH * 2, false, IMAGE_FORMAT)
+	var face_texture_paths := [
+			texture_north_path,
+			texture_east_path,
+			texture_south_path,
+			texture_west_path,
+			effective_automap_texture_path(),
+			effective_automap_texture_path()
+	]
+	for face_number in 6:
+		var texture_to_add = load(face_texture_paths[face_number])
+		var image_to_add : Image
+		if texture_to_add == null:
+			push_error("Failed to load “%s”. Using “%s” as a fallback…" % [face_texture_paths[face_number], MISSING_TEXTURE_PATH])
+			texture_to_add = load(MISSING_TEXTURE_PATH)
+			if texture_to_add == null:
+				push_error("Failed to load fallback texture. Using a completely black square as a fallback…")
+				image_to_add = Image.new()
+				image_to_add.create(VSwap.WALL_LENGTH, VSwap.WALL_LENGTH, false, Image.FORMAT_L8)
+				image_to_add.fill(Color.black)
+		if image_to_add == null:
+			image_to_add = texture_to_add.get_data()
+		
+		var row : int = face_number % 3
+		var column : int = 0 if face_number < 3 else 1
+		image_to_add.resize(VSwap.WALL_LENGTH, VSwap.WALL_LENGTH, Image.INTERPOLATE_NEAREST)
+		if face_number == 4:
+			# Without this, the top texture would appear upside down.
+			image_to_add.unlock()
+			image_to_add.flip_x()
+			image_to_add.flip_y()
+			image_to_add.lock()
+		albedo_image.blit_rect(
+				image_to_add,
+				Rect2(0, 0, VSwap.WALL_LENGTH, VSwap.WALL_LENGTH),
+				Vector2(row * VSwap.WALL_LENGTH, column * VSwap.WALL_LENGTH)
+		)
+	var albedo_texture := ImageTexture.new()
+	albedo_texture.create_from_image(albedo_image, Texture.FLAGS_DEFAULT & ~Texture.FLAG_FILTER)
+	
+	var new_material := SpatialMaterial.new()
+	new_material.flags_unshaded = true
+	new_material.albedo_texture = albedo_texture
+	set_surface_material(0, new_material)
 
-func set_texture_north(new_texture_north : Texture) -> void:
-	$North.texture = new_texture_north
 
-func get_texture_north() -> Texture:
-	return $North.texture
+func _ready() -> void:
+	_update_material()
 
-func set_texture_south(new_texture_south : Texture) -> void:
-	$South.texture = new_texture_south
 
-func get_texture_south() -> Texture:
-	return $South.texture
+func set_texture_east_path(new_texture_path : String) -> void:
+	texture_east_path = new_texture_path
+	_update_material()
 
-func set_texture_west(new_texture_west : Texture) -> void:
-	$West.texture = new_texture_west
 
-func get_texture_west() -> Texture:
-	return $West.texture
+func set_texture_north_path(new_texture_path : String) -> void:
+	texture_north_path = new_texture_path
+	_update_material()
+
+
+func set_texture_south_path(new_texture_path : String) -> void:
+	texture_south_path = new_texture_path
+	_update_material()
+
+
+func set_texture_west_path(new_texture_path : String) -> void:
+	texture_west_path = new_texture_path
+	_update_material()
--- project/wolf_editing_tools/scenes_and_scripts/test_3d.gd
+++ project/wolf_editing_tools/scenes_and_scripts/test_3d.gd
@@ -4,5 +4,9 @@ extends Node
 onready var tile := $Tile
 
 
+func _ready() -> void:
+	$TextureRect.texture = tile.get_surface_material(0).albedo_texture
+	print(tile.get_surface_material(0))
+
 func _physics_process(delta: float) -> void:
 	tile.rotate(Vector3.UP, PI / 8 * delta)
