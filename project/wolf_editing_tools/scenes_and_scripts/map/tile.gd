1f5ba4669e02e483cd3fa69d76b8fdd3fe63d98d
Jason Yundt <jason@jasonyundt.email>
2022-09-28 21:27:31 -0400

    Revert "Stop saved Tiles from including unlicensed content"
    This reverts commit dc9ae3d85b4ccc4739c1f628aace661d640f9089.
    
    Using external resources isn’t the way to fix this problem. An external
    resource must exist before the scene is loaded. The scene must be loaded
    before the script starts, but the script is needed to generate the
    external resource. Godot would crash before the script was able to
    generate the files that Godot was complaining it couldn’t find.

--- project/wolf_editing_tools/scenes_and_scripts/map/tile.gd
+++ project/wolf_editing_tools/scenes_and_scripts/map/tile.gd
@@ -2,31 +2,14 @@ tool
 extends MeshInstance
 
 
-const AssetGenerator := preload("res://wolf_editing_tools/scenes_and_scripts/asset_generator.gd")
 const VSwap := preload("res://wolf_editing_tools/scenes_and_scripts/file_formats/v_swap.gd")
-# TODO: There’s a chance that this won’t end in .tex. See asset_generator.gd’s save_texture() function.
 const MISSING_TEXTURE_PATH := "res://wolf_editing_tools/generated/art/missing_texture.tex"
 const IMAGE_FORMAT := Image.FORMAT_RGB8
-const MATERIAL_DIR := "user://art/walls"
 
 export(String, FILE, "*.tex") var texture_east_path : String = MISSING_TEXTURE_PATH setget set_texture_east_path
 export(String, FILE, "*.tex") var texture_north_path : String = MISSING_TEXTURE_PATH setget set_texture_north_path
 export(String, FILE, "*.tex") var texture_south_path : String = MISSING_TEXTURE_PATH setget set_texture_south_path
 export(String, FILE, "*.tex") var texture_west_path : String = MISSING_TEXTURE_PATH setget set_texture_west_path
-
-
-func face_texture_paths_hash(face_texture_paths : Array) -> int:
-	var file_content_hashes := []
-	file_content_hashes.resize(len(face_texture_paths))
-	var file := File.new()
-	for i in len(face_texture_paths):
-		var sha256 := file.get_sha256(face_texture_paths[i])
-		if sha256.empty():
-			push_warning("Failed to open “%s”. Hashing path instead of file contents…" % [face_texture_paths[i]])
-			file_content_hashes[i] = hash(face_texture_paths[i])
-		else:
-			file_content_hashes[i] = sha256
-	return hash(file_content_hashes)
 
 
 func effective_automap_texture_path() -> String:
@@ -44,54 +27,41 @@ func _update_material() -> void:
 			effective_automap_texture_path(),
 			effective_automap_texture_path()
 	]
-	var material_path = "%s/%x.res" % [MATERIAL_DIR, face_texture_paths_hash(face_texture_paths)]
-	var material = load(material_path)
-	if material == null:
-		for face_number in 6:
-			var texture_to_add = load(face_texture_paths[face_number])
-			var image_to_add : Image
+	for face_number in 6:
+		var texture_to_add = load(face_texture_paths[face_number])
+		var image_to_add : Image
+		if texture_to_add == null:
+			push_error("Failed to load “%s”. Using “%s” as a fallback…" % [face_texture_paths[face_number], MISSING_TEXTURE_PATH])
+			texture_to_add = load(MISSING_TEXTURE_PATH)
 			if texture_to_add == null:
-				push_error("Failed to load “%s”. Using “%s” as a fallback…" % [face_texture_paths[face_number], MISSING_TEXTURE_PATH])
-				texture_to_add = load(MISSING_TEXTURE_PATH)
-				if texture_to_add == null:
-					push_error("Failed to load fallback texture. Using a completely black square as a fallback…")
-					image_to_add = Image.new()
-					image_to_add.create(VSwap.WALL_LENGTH, VSwap.WALL_LENGTH, false, Image.FORMAT_L8)
-					image_to_add.fill(Color.black)
-			if image_to_add == null:
-				image_to_add = texture_to_add.get_data()
-			
-			var row : int = face_number % 3
-			var column : int = 0 if face_number < 3 else 1
-			image_to_add.resize(VSwap.WALL_LENGTH, VSwap.WALL_LENGTH, Image.INTERPOLATE_NEAREST)
-			if face_number == 4:
-				# Without this, the top texture would appear upside down.
-				image_to_add.unlock()
-				image_to_add.flip_x()
-				image_to_add.flip_y()
-				image_to_add.lock()
-			albedo_image.blit_rect(
-					image_to_add,
-					Rect2(0, 0, VSwap.WALL_LENGTH, VSwap.WALL_LENGTH),
-					Vector2(row * VSwap.WALL_LENGTH, column * VSwap.WALL_LENGTH)
-			)
-		var albedo_texture := ImageTexture.new()
-		albedo_texture.create_from_image(albedo_image, Texture.FLAGS_DEFAULT & ~Texture.FLAG_FILTER)
+				push_error("Failed to load fallback texture. Using a completely black square as a fallback…")
+				image_to_add = Image.new()
+				image_to_add.create(VSwap.WALL_LENGTH, VSwap.WALL_LENGTH, false, Image.FORMAT_L8)
+				image_to_add.fill(Color.black)
+		if image_to_add == null:
+			image_to_add = texture_to_add.get_data()
 		
-		material = SpatialMaterial.new()
-		material.flags_unshaded = true
-		material.albedo_texture = albedo_texture
-		
-		AssetGenerator.recursively_create_directory(MATERIAL_DIR)
-		if ResourceSaver.save(material_path, material) != OK:
-			push_error("Failed to save material “%s”" % [material])
-		# This ensures that Godot uses an external resource rather than an embedded one. If an
-		# embeded resource was used, then the albedo_image will be embedded into the scene file. The
-		# albedo_image is likely to contain copyrighted textures from Wolfenstein 3D, and users are
-		# unlikely to have a license for that content. Scene files are the source files for maps, so
-		# it’s important that end users are able to redistribute them.
-		material = load(material_path)
-	set_surface_material(0, material)
+		var row : int = face_number % 3
+		var column : int = 0 if face_number < 3 else 1
+		image_to_add.resize(VSwap.WALL_LENGTH, VSwap.WALL_LENGTH, Image.INTERPOLATE_NEAREST)
+		if face_number == 4:
+			# Without this, the top texture would appear upside down.
+			image_to_add.unlock()
+			image_to_add.flip_x()
+			image_to_add.flip_y()
+			image_to_add.lock()
+		albedo_image.blit_rect(
+				image_to_add,
+				Rect2(0, 0, VSwap.WALL_LENGTH, VSwap.WALL_LENGTH),
+				Vector2(row * VSwap.WALL_LENGTH, column * VSwap.WALL_LENGTH)
+		)
+	var albedo_texture := ImageTexture.new()
+	albedo_texture.create_from_image(albedo_image, Texture.FLAGS_DEFAULT & ~Texture.FLAG_FILTER)
+	
+	var new_material := SpatialMaterial.new()
+	new_material.flags_unshaded = true
+	new_material.albedo_texture = albedo_texture
+	set_surface_material(0, new_material)
 
 
 func _ready() -> void:
--- project/wolf_editing_tools/scenes_and_scripts/test_3d.gd
+++ project/wolf_editing_tools/scenes_and_scripts/test_3d.gd
@@ -6,6 +6,7 @@ onready var tile := $Tile
 
 func _ready() -> void:
 	$TextureRect.texture = tile.get_surface_material(0).albedo_texture
+	print(tile.get_surface_material(0))
 
 func _physics_process(delta: float) -> void:
 	tile.rotate(Vector3.UP, PI / 8 * delta)
