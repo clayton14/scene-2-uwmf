24f812335aeb8ea940e7b8d826d758523b743892
Jason Yundt <jason@jasonyundt.email>
2022-10-01 19:14:38 -0400

    Add texture_east to Tile

--- project/wolf_editing_tools/scenes_and_scripts/map/tile.gd
+++ project/wolf_editing_tools/scenes_and_scripts/map/tile.gd
@@ -1,1 +1,20 @@
+tool
 extends Spatial
+
+
+export var texture_east : Texture setget set_texture_east
+
+
+func set_texture_east(new_texture_east : Texture) -> void:
+	var wall_east := $WallEast
+	if wall_east is MeshInstance:
+		if new_texture_east == null:
+			wall_east.material_override = null
+		else:
+			var new_material := SpatialMaterial.new()
+			new_material.flags_unshaded = true
+			new_material.albedo_texture = new_texture_east
+			wall_east.material_override = new_material
+	else:
+		push_error("wall_east wasn’t a MeshInstance.")
+	texture_east = new_texture_east
