93a9cdb22c62ac9943f78ded40e47d9510fd13d0
Jason Yundt <jason@jasonyundt.email>
2022-10-01 16:53:27 -0400

    Move test scenes into a test directory

--- project/wolf_editing_tools/scenes_and_scripts/test/instance_outside_of_scene_tree.gd
+++ project/wolf_editing_tools/scenes_and_scripts/test/instance_outside_of_scene_tree.gd
@@ -2,7 +2,7 @@ extends TextureRect
 
 
 func _ready() -> void:
-	var packed_scene := preload("res://wolf_editing_tools/scenes_and_scripts/test_3d.tscn")
+	var packed_scene := preload("res://wolf_editing_tools/scenes_and_scripts/test/scene_with_tiles.tscn")
 	var scene := packed_scene.instance()
 	var tile := scene.get_node("Tile")
 	if tile is Tile:
