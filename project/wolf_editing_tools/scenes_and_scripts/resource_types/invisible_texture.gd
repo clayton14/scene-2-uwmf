6452d5bf5fc8bfa369b2a2c98fec30096500973d
Jason Yundt <jason@jasonyundt.email>
2022-10-02 11:18:52 -0400

    Prevent SingleColorTexture.color from being transparent

--- project/wolf_editing_tools/scenes_and_scripts/resource_types/invisible_texture.gd
+++ project/wolf_editing_tools/scenes_and_scripts/resource_types/invisible_texture.gd
@@ -1,10 +1,10 @@
-tool
+#tool
 class_name InvisibleTexture
 extends SingleColorTexture
 
 
 func _init() -> void:
-	.set_color(Color.transparent)
+	._set_color_no_verify(Color.transparent)
 
 
 func set_color(_color) -> void:
@@ -13,3 +13,7 @@ func set_color(_color) -> void:
 
 func to_uwmf() -> String:
 	return '"-"'
+
+
+static func color_usage() -> int:
+	return .color_usage() & ~PROPERTY_USAGE_STORAGE
--- project/wolf_editing_tools/scenes_and_scripts/resource_types/single_color_texture.gd
+++ project/wolf_editing_tools/scenes_and_scripts/resource_types/single_color_texture.gd
@@ -1,4 +1,4 @@
-tool
+#tool
 class_name SingleColorTexture
 extends ImageTexture
 
@@ -6,14 +6,11 @@ extends ImageTexture
 signal color_changed
 
 
-export var color : Color setget set_color, get_color
-
-
 func _init() -> void:
 	set_color(Color.white)
 
 
-func set_color(new_color : Color) -> void:
+func _set_color_no_verify(new_color : Color) -> void:
 	var new_image = Image.new()
 	new_image.create(1, 1, false, Util.TILE_IMAGE_FORMAT)
 	new_image.lock()
@@ -23,6 +20,13 @@ func set_color(new_color : Color) -> void:
 	emit_signal("color_changed")
 
 
+func set_color(new_color : Color) -> void:
+	if new_color.a != 1.0:
+		push_warning("The UWMF doesn’t support transparent color textures. Resetting alpha to 1.0…")
+		new_color.a = 1.0
+	_set_color_no_verify(new_color)
+
+
 func get_color() -> Color:
 	var return_value : Color
 	var data := get_data()
@@ -30,3 +34,33 @@ func get_color() -> Color:
 	return_value = data.get_pixelv(Vector2.ZERO)
 	data.unlock()
 	return return_value
+
+
+static func color_usage() -> int:
+	return PROPERTY_USAGE_DEFAULT
+
+
+func _get_property_list() -> Array:
+	return [
+		{
+			"name" : "color",
+			"type" : typeof(Color.white),
+			"usage" : color_usage()
+		}
+	]
+
+
+func _get(property):
+	if property is String and property == "color":
+		return get_color()
+	else:
+		return null
+
+
+func _set(property, value) -> bool:
+	if property is String and property == "color":
+		if value is Color:
+			set_color(value)
+		else:
+			push_error("“%s” is not a Color." % [value])
+	return false
